12c2c6ef0d492691c00ae30897c629d8c967de85
GavryaAK <alina.gavrya@mail.ru>
2022-04-05 22:15:29 +0300

    пересобрала ddsdata и ddsdataex своей версией fastddsgen, ошибки исчезли

---
+++ TypeTopicsDDS/TypeTopics.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "DDSAlarm/DDSAlarm.h"
+#include "DDSAlarmEx/DDSAlarmEx.h"
+#include "DDSData/DDSData.h"
+#include "DDSDataEx/DDSDataEx.h"
+#include "TopicCommand/TopicCommand.h"
+#include "TopicStatus/TopicStatus.h"
---
+++ TypeTopicsDDS/TypeTopicsPubSubTypes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "DDSAlarm/DDSAlarmPubSubTypes.h"
+#include "DDSAlarmEx/DDSAlarmExPubSubTypes.h"
+#include "DDSData/DDSDataPubSubTypes.h"
+#include "DDSDataEx/DDSDataExPubSubTypes.h"
+#include "TopicCommand/TopicCommand.h"
+#include "TopicStatus/TopicStatus.h"
